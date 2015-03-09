bf3222178cb76ec2604a869117d01a750a8d3d85
Kevin A Jasieniecki <kevinjasieniecki@Kevins-MacBook-Pro-2.local>
2015-03-09 03:18:25 -0500

    skeleton commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # react-todo
 Todo list created w/ reactjs and flask
+
+
+Run with python server.js, open on localhost:3000
---
+++ server.py
@@ -0,0 +1,18 @@
+import json
+from flask import Flask, Response, request
+
+app = Flask(__name__, static_url_path='', static_folder='public')
+app.add_url_rule('/', 'root', lambda: app.send_static_file('index.html'))
+
+
+@app.route('/todos.json', methods=['GET', 'POST'])
+def todo_handler():
+    todos = []
+
+    if request.method == 'POST':
+	todos = (request.get_json())
+
+    return Response(json.dumps(todos), mimetype='application/json')
+
+if __name__ == '__main__':
+    app.run(port=3000, debug=True)
