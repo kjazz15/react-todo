1e426db3b15e7a6988f3df9c49716c9e02ecb75d
Kevin A Jasieniecki <kevinjasieniecki@Kevins-MacBook-Pro-2.local>
2015-03-09 23:03:56 -0500

    cleaned up

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from flask import Flask, Response, request
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def main():
+    app.send_static_file('index.html')
+
 @app.route('/todos.json', methods=['GET', 'POST'])
 def todo():
     todos = []
@@ -12,6 +17,5 @@ def todo():
 
     return Response(json.dumps(todos), mimetype='application/json')
 
-app.add_url_rule('/', 'root', lambda: app.send_static_file('index.html'))
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
