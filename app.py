28c8815ba7e576063c3bf475018edd912ef788e4
Kevin A Jasieniecki <kevinjasieniecki@Kevins-MacBook-Pro-2.local>
2015-03-13 18:24:23 -0500

    this should work

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 import json
 from flask import Flask, Response, request
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def main():
-    app.send_static_file('index.html')
+app = Flask(__name__, static_url_path='', static_folder="static")
+app.add_url_rule('/', 'root', lambda: app.send_static_file('index.html'))
 
 @app.route('/todos.json', methods=['GET', 'POST'])
 def todo():
